7fdf88832a52deac5c66147c76e18094404215e8
DominicPage <65826540+DominicPage@users.noreply.github.com>
2023-06-13 23:04:42 +0100

    Added several new pages
    Refined the performance on mobile devices for some pages. Added several other templates to continue working from. 
    Main elements nearing completion and ready for review are about and news pages

--- animate.js
+++ animate.js
@@ -13,7 +13,22 @@ serviceCards.forEach((card) => {
     observer.observe(card);
 });
 
-document.querySelectorAll('section h2').forEach(el => {
+const newsCards = document.querySelectorAll('.full-article');
+
+const newsObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.style.visibility = 'visible';
+            entry.target.style.animationPlayState = 'running';
+        }
+    });
+});
+
+newsCards.forEach((card) => {
+    newsObserver.observe(card);
+});
+
+document.querySelectorAll('.section-header').forEach(el => {
     let observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
@@ -25,3 +40,27 @@ document.querySelectorAll('section h2').forEach(el => {
 
     observer.observe(el);
 });
+
+window.addEventListener('scroll', function() {
+    let timelineEvents = document.querySelectorAll('.timeline-event');
+    timelineEvents.forEach(event => {
+      let positionFromTop = event.getBoundingClientRect().top;
+      if(positionFromTop - window.innerHeight <= 0) {
+        event.style.animation = 'comeIn 1s ease-out forwards';
+      }
+    });
+  });
+
+  document.querySelectorAll('.member-card').forEach(card => {
+    card.addEventListener('click', function() {
+      let innerCard = this.querySelector('.card-inner');
+      if(innerCard.style.transform === 'rotateY(180deg)') {
+        innerCard.style.transform = 'rotateY(0deg)';
+      } else {
+        innerCard.style.transform = 'rotateY(180deg)';
+      }
+    });
+  });
+
+
+  
